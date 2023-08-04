1dfb58eb9d6426d1f6a9b791c8be321a85e6839b
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-08-04 10:01:41 +0200

    Réfacto de main et menu

--- src/Menu.java
+++ src/Menu.java
@@ -16,6 +16,39 @@ public class Menu {
         return scanner.nextInt();
     }
 
+    public void createCharacter(){
+        boolean onOff;
+        System.out.println("What type of character would you like to create? \r\n 1: Warrior 2: Magician");
+        int characterType = this.readOption();
+        if (characterType == 1) {
+            Warrior character = this.createWarrior();
+            System.out.println("Warrior created !!!");
+            System.out.println("Would you like to see your stats ? \r\n 1:Yes 2:No 3:Modify");
+            int option = this.readOption();
+            if (option == 1){
+                System.out.println(character.toString());
+            } else if (option == 2) {
+                onOff = false;
+            } else if (option == 3) {
+                System.out.println("modfier en cours de fabrication");
+            }
+        } else if (characterType == 2) {
+            Magician character = this.createMagician();
+            System.out.println("Magician created !!!");
+            System.out.println("Would you like to see your stats ? \r\n 1:Yes 2:No 3:Modify");
+            int option = this.readOption();
+            if (option == 1){
+                System.out.println(character.toString());
+            } else if (option == 2) {
+                onOff = false;
+            } else if (option == 3) {
+                System.out.println("modfier en cours de fabrication");
+            }
+        } else {
+            System.out.println("Invalid option. Please try again");
+        }
+    }
+
     public Warrior createWarrior() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the name of Warrior's:");
