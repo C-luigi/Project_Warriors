0751dec8dc4ad08ca25f6cb8235cd91fd1b975df
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-08-07 14:57:19 +0200

    modification du personnage à la fin de création

--- src/Menu.java
+++ src/Menu.java
@@ -39,15 +39,23 @@ public class Menu {
 
     public String handleCharacterSheet(Object character) {
         if (character instanceof Warrior || character instanceof Magician) {
-            System.out.println(character.getClass().getSimpleName() + " created !!!");
-            System.out.println("Would you like to see the character sheet ? \r\n 1:Yes 2:No 3:Modify");
-            int seeSheet = this.readOption();
+            int seeSheet;
             String characterSheet = character.toString();
-            if (seeSheet == 1 || seeSheet == 2) {
-                System.out.println(characterSheet);
-            } else if (seeSheet == 3) {
-                System.out.println("Modification feature is under construction");
-            }
+            do {
+                System.out.println(character.getClass().getSimpleName() + " created !!!");
+                System.out.println("Would you like to see the character sheet ? \r\n 1:Yes 2:No 3:Modify");
+                seeSheet = this.readOption();
+                characterSheet = character.toString();
+                if (seeSheet == 1 || seeSheet == 3) {
+                    if (seeSheet == 1) {
+                        System.out.println(characterSheet);
+                    } else {
+                        modifyCharacter(character);
+                    }
+                } else if (seeSheet != 2) {
+                    System.out.println("Invalid option. Please try again");
+                }
+            } while (seeSheet != 2);
             return characterSheet;
         }
         else{
@@ -63,14 +71,17 @@ public class Menu {
         System.out.println("Enter the Warrior standard of living (between 5 and 10):");
         while (healthPoints < 5 || healthPoints > 10) {
             healthPoints = scanner.nextInt();
-            System.out.println("Invalid input! Please enter a value between 5 and 10.");
+            if (healthPoints < 5 || healthPoints > 10) {
+                System.out.println("Invalid input! Please enter a value between 5 and 10.");
+            }
         }
         int strongPoints = 0;
         System.out.println("Enter the Warrior attack strength (between 5 and 10):");
         while (strongPoints < 5 || strongPoints > 10) {
             strongPoints = scanner.nextInt();
-            System.out.println("Invalid input! Please enter a value between 5 and 10.");
-        }
+            if (strongPoints < 5 || strongPoints > 10) {
+                System.out.println("Invalid input! Please enter a value between 5 and 10.");
+            }}
 
         return new Warrior(name, healthPoints, strongPoints, new Weapon("fist",0));
     }
@@ -83,17 +94,96 @@ public class Menu {
         System.out.println("Enter the Magician standard of living (between 3 and 6):");
         while (healthPoints < 3 || healthPoints > 6) {
             healthPoints = scanner.nextInt();
-            System.out.println("Invalid input! Please enter a value between 3 and 6.");
+            if (healthPoints < 3 || healthPoints > 6) {
+                System.out.println("Invalid input! Please enter a value between 3 and 6.");
+            }
         }
         int strongPoints = 0;
         System.out.println("Enter the Magician attack strength (between 8 and 15):");
         while (strongPoints < 8 || strongPoints > 15) {
             strongPoints = scanner.nextInt();
-            System.out.println("Invalid input! Please enter a value between 8 and 15.");
+            if (strongPoints < 8 || strongPoints > 15) {
+                System.out.println("Invalid input! Please enter a value between 8 and 15.");
+            }
         }
 
         return new Magician(name, healthPoints, strongPoints, new Spell("fist",0));
     }
+
+    public void modifyCharacter(Object character) {
+        if (character instanceof Warrior) {
+            modifyWarrior((Warrior) character);
+        } else if (character instanceof Magician) {
+            modifyMagician((Magician) character);
+        } else {
+            System.out.println("Unknown character type.");
+        }
+    }
+
+    public void modifyWarrior(Warrior warrior) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like to modify?");
+        System.out.println("1. Name");
+        System.out.println("2. Standard of Living");
+        System.out.println("3. Attack Strength");
+        System.out.println("4. Weapon");
+
+        int option = scanner.nextInt();
+        switch(option) {
+            case 1:
+                System.out.println("Enter the new name:");
+                warrior.setName(scanner.next());
+                break;
+            case 2:
+                System.out.println("Enter the new standard of living:");
+                warrior.setHealthPoints(scanner.nextInt());
+                break;
+            case 3:
+                System.out.println("Enter the new attack strength:");
+                warrior.setStrongPoints(scanner.nextInt());
+                break;
+            case 4:
+                System.out.println("Enter the new weapon:");
+                warrior.setWeapon(createWeapon());
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+
+    public void modifyMagician(Magician magician) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like to modify?");
+        System.out.println("1. Name");
+        System.out.println("2. Standard of Living");
+        System.out.println("3. Attack Strength");
+        System.out.println("4. Spell");
+
+        int option = scanner.nextInt();
+        switch(option) {
+            case 1:
+                System.out.println("Enter the new name:");
+                magician.setName(scanner.next());
+                break;
+            case 2:
+                System.out.println("Enter the new standard of living:");
+                magician.setHealthPoints(scanner.nextInt());
+                break;
+            case 3:
+                System.out.println("Enter the new attack strength:");
+                magician.setStrongPoints(scanner.nextInt());
+                break;
+            case 4:
+                System.out.println("Enter the new spell:");
+                magician.setSpell(createSpell());
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+
 
     public Weapon createWeapon() {
         Scanner scanner = new Scanner(System.in);
