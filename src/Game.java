4d25af46b4b730dd68d3ee68901db1af60ab55e2
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-08-08 08:42:35 +0200

    update exception "CharacterOutOfBoardException"

--- src/Game.java
+++ src/Game.java
@@ -23,7 +23,7 @@ public class Game {
 
     public void move(int steps) throws CharacterOutOfBoardException {
         this.currentPosition += steps;
-        if (this.currentPosition > boardSize) {
+        if (this.currentPosition >= boardSize) {
             throw new CharacterOutOfBoardException("The character has moved off the board");
         }
     }
@@ -33,30 +33,28 @@ public class Game {
         Menu menu = new Menu();
         while (true) {
             System.out.println("Press any touch and press enter for play");
-            int captureInput = menu.readOption();
+            menu.readOption();
             int roll = roll();
             System.out.println("You rolled a " + roll);
             try {
                 move(roll);
                 System.out.println(character + " is now at square " + currentPosition + " / " + boardSize);
-                if (currentPosition == boardSize) {
-                    System.out.println("Congratulations to you! You've reached the end of the board!");
-                    System.out.println("Would you like to start the game again ? \r\n 1: Yes 2: No");
-                    int option = menu.readOption();
-                    if (option == 1) {
-                        currentPosition = 1;
-                        System.out.println("The game restart");
-                    }
-                    else if (option == 2){
-                        System.out.println("Thank you for playing");
-                        break;
-                    }
-                    else {
-                        System.out.println("Invalid option. Please try again");
-                    }
-                }
             } catch (CharacterOutOfBoardException e) {
                 System.out.println(e.getMessage());
+                System.out.println("Congratulations to you! You've reached the end of the board!");
+                System.out.println("Would you like to start the game again ? \r\n 1: Yes 2: No");
+                int option = menu.readOption();
+                if (option == 1) {
+                    currentPosition = 1;
+                    System.out.println("The game restart");
+                }
+                else if (option == 2){
+                    System.out.println("Thank you for playing");
+                    break;
+                }
+                else {
+                    System.out.println("Invalid option. Please try again");
+                }
             }
         }
     }
